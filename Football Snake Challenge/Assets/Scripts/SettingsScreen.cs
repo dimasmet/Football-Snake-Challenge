de023b6e129b807b856c1b78a42861b0148e16d8
Dmitriy <dimasmet@mail.ru>
2024-08-26 16:23:32 +0300

    Snake White

--- Football Snake Challenge/Assets/Scripts/SettingsScreen.cs
+++ Football Snake Challenge/Assets/Scripts/SettingsScreen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.iOS;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,12 +11,43 @@ public class SettingsScreen : MonoBehaviour
     [SerializeField] private Button _rateUs;
     [SerializeField] private Button _termsOfUse;
     [SerializeField] private Button _privacyBtn;
+    [SerializeField] private Button _closePanelTextBtn;
+
+    [SerializeField] private GameObject _panel;
+    [SerializeField] private GameObject _privacy;
+    [SerializeField] private GameObject _termsOfUsePanel;
 
     private void Awake()
     {
+        _rateUs.onClick.AddListener(() =>
+        {
+            Device.RequestStoreReview();
+        });
+
         _backBtn.onClick.AddListener(() =>
         {
             ScreensController.I.ShowPanel(ScreensController.ScreenName.Menu);
+        });
+
+        _termsOfUse.onClick.AddListener(() =>
+        {
+            _panel.SetActive(true);
+
+            _privacy.SetActive(false);
+            _termsOfUsePanel.SetActive(true);
+        });
+
+        _privacyBtn.onClick.AddListener(() =>
+        {
+            _panel.SetActive(true);
+
+            _privacy.SetActive(true);
+            _termsOfUsePanel.SetActive(false);
+        });
+
+        _closePanelTextBtn.onClick.AddListener(() =>
+        {
+            _panel.SetActive(false);
         });
     }
 }
